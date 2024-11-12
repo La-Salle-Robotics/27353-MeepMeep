048c5aed41c4ee28f120548b95ea5c43851094be
jakelovescoding <jakejin2016@gmail.com>
2024-11-12 15:15:35 -0800

    right side traj

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,18 +12,25 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(25, 50, Math.PI, Math.PI, 16.5)
+                .setConstraints(50, 50, Math.PI, Math.PI, 16.5)
+                .setDimensions(17.785,17.323)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, -61.5, Math.toRadians(90)))
-                .waitSeconds(3)
-                .lineToY(38)
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(23.5, -61.5, Math.toRadians(-90)))
                 .waitSeconds(1)
-                .strafeTo(new Vector2d(38, 38))
-                        .waitSeconds(1)
-                        .lineToY(-50)
-                        .waitSeconds(1)
-                        .strafeTo(new Vector2d(-36, -60))
+                .strafeTo(new Vector2d(0, -34)) // score preload
+                .strafeTo(new Vector2d(38,-34)) // go to push
+                .strafeTo(new Vector2d(38,-10))
+                .strafeToLinearHeading(new Vector2d(47,-10),Math.toRadians(0))
+
+                .strafeTo(new Vector2d(47,-52))
+                .strafeTo(new Vector2d(47,-10)) // second sample
+                .strafeTo(new Vector2d(56,-10))
+                .strafeTo(new Vector2d(56,-52))
+                .strafeToLinearHeading(new Vector2d(38,-58), Math.toRadians(90)) // grab first specimen
+                .strafeToLinearHeading(new Vector2d(0,-34), Math.toRadians(-90)) // score first specimen
+                .strafeToLinearHeading(new Vector2d(38,-58), Math.toRadians(90)) // grab second specimen
+                .strafeToLinearHeading(new Vector2d(0,-34), Math.toRadians(-90)) // score second specimen
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
